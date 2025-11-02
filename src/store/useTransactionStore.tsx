6f4cb4d9bedb99b09a4fdf762075c7acc5509678
MagdalenaMie <madeladesign@gmail.com>
2025-11-02 15:06:24 +0100

    feat: add Zustand store for transactions

---
+++ src/store/useTransactionStore.tsx
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+import type { Transaction } from "../types/transaction";
+
+type TransactionState = {
+  transactions: Transaction[];
+  addTransaction: (transaction: Transaction) => void;
+};
+
+export const useTransactionStore = create<TransactionState>((set) => ({
+  transactions: [],
+  addTransaction: (transaction) =>
+    set((state) => ({
+      transactions: [...state.transactions, transaction],
+    })),
+}));
