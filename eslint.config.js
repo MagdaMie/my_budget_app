025ccb17b50b81a5a55ec91c02184db1e4b4fa7b
MagdalenaMie <madeladesign@gmail.com>
2025-11-05 14:58:01 +0100

    chore: migrate ESLint configuration to flat config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,67 +1,71 @@
-import js from "@eslint/js";
-import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+export default [
+
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      parser: tseslint.parser,
       globals: globals.browser,
+      parser: tseslint.parser,
     },
     plugins: {
       react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       prettier: prettierPlugin,
-      "@typescript-eslint": tseslint.plugin,
     },
-    extends: [js.configs.recommended, prettierConfig],
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
-    rules: {
-      ...tseslint.configs.recommended.rules,
+    rules: { 
       ...react.configs.recommended.rules,
-      ...reactHooks.configs["recommended-latest"].rules,
-      ...reactRefresh.configs.vite.rules,
-      "prettier/prettier": [
-        "error",
+      ...reactHooks.configs['recommended-latest'].rules,
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-key': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': [
+        'error',
         {
           singleQuote: false,
           semi: true,
-          trailingComma: "all",
+          trailingComma: 'all',
           printWidth: 100,
           tabWidth: 2,
-          endOfLine: "auto",
+          endOfLine: 'auto',
         },
       ],
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
       ],
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "no-console": "warn",
-      eqeqeq: ["error", "always"],
-      curly: "error",
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+      eqeqeq: ['error', 'always'],
+      curly: 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
     },
   },
-]);
+];
