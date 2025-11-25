10ae415087fa28d9074040756c607fb929caf7a9
MagdalenaMie <madeladesign@gmail.com>
2025-11-25 10:45:08 +0100

    Revert "feat: display AddTransactionCard centered on Home page"
    This reverts commit 0869864e8445fb9bed4be04b096154247a07c851.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,9 @@
 import AddTransactionCard from "./components/AddTransactionCard";
-import { Box } from "@mui/material";
-
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <AddTransactionCard />
-      </Box>
+      <AddTransactionCard />
     </>
   );
 };
