ac69250edcfd5cb4165560f843d9ae90b0202aef
MagdalenaMie <madeladesign@gmail.com>
2025-11-25 10:56:22 +0100

    Remove AddTransactionCard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,5 @@
-import AddTransactionCard from "./components/AddTransactionCard";
-import { Box } from "@mui/material";
-
 const Home = () => {
-  return (
-    <>
-      <h1>Home</h1>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <AddTransactionCard />
-      </Box>
-    </>
-  );
+  return <h1>Home</h1>;
 };
 
 export default Home;
