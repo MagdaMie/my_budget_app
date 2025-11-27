3448ff498ddf9b04f19f1dfbd65355caeec8f3e0
MagdalenaMie <madeladesign@gmail.com>
2025-11-27 09:16:25 +0100

    feat: add transaction filtering by category with "All" option

--- src/pages/components/TransactonsList.tsx
+++ src/pages/components/TransactonsList.tsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import { Box } from "@mui/material";
 import {
   useTransactionStore,
   type Transaction,
 } from "../../store/transactionStore";
 import TransactionCard from "./TransactionCart";
+import AppSelect from "./AppSelect";
+import { categories } from "../../data/categories";
 
 const TransactionsList = () => {
   const transactions = useTransactionStore((state) => state.transactions);
+
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+
+  const fillteredTransactions = selectedCategory
+    ? transactions.filter((t) => t.category === selectedCategory)
+    : transactions;
+
   return (
-    <Box sx={{ display:"flex", flexDirection:"column", alignItems:"center", p :2}}>
-      {transactions.map((t: Transaction) => (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        p: 2,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          py: 2,
+          width: "100%",
+          maxWidth: "600px",
+        }}
+      >
+        <AppSelect
+          label="Filter by category"
+          options={[{ value: "", label: "All" }, ...categories]}
+          value={selectedCategory}
+          onChange={(value) => setSelectedCategory(value)}
+        />
+      </Box>
+
+      {fillteredTransactions.map((t: Transaction) => (
         <TransactionCard key={t.id} transaction={t} />
       ))}
     </Box>
